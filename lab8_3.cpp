2734fc4bc4c1698ea22bdfb337e583e6407d8360
Unknown <trtrinw@gmail.com>
2018-01-12 14:56:38 +0700

    ข้อ3

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -4,16 +4,50 @@ using namespace std;
 int main () {
         // สร้างตัวแปร array ชื่อ calendar เพื่อสร้างที่เก็บปฏิทิน
         // calendar[5][7]
+        int claendar[6][7];
 
 
         // เขียนโปรแกรมแสดงปฏิทินเดือน ธันวาคม /2560
         // วันที่ไม่ใช่วันของเดือน ธันวาคมให้แสดงด้วย x
-
-
+        claendar[0][5] = 1;
+        claendar[0][6] = 2;
+        claendar[1][0] = 3;
+        claendar[1][1] = 4;
+        claendar[1][2] = 5;
+        claendar[1][3] = 6;
+        claendar[1][5] = 7;
+        claendar[2][6] = 8;
+        claendar[1][6] = 9;
+        claendar[2][0] = 10;
+        claendar[2][1] = 11;
+        claendar[2][2] = 12;
+        claendar[2][3] = 13;
+        claendar[2][4] = 14;
+        claendar[2][5] = 15;
+        claendar[2][6] = 16;
+        claendar[3][0] = 17;
+        claendar[3][1] = 18;
+        claendar[3][2] = 19;
+        claendar[3][3] = 20;
+        claendar[3][4] = 21;
+        claendar[3][5] = 22;
+        claendar[3][6] = 23;
+        claendar[4][0] = 24;
+        claendar[4][1] = 25;
+        claendar[4][2] = 26;
+        claendar[4][3] = 27;
+        claendar[4][4] = 28;
+        claendar[4][5] = 29;
+        claendar[4][6] = 30;
+        claendar[5][0] = 31;
         // เขียนโปรแกรมเพื่อรับวันจากผู้ใช้ แล้วแสดงเฉพาะวันที่นั้น
         // 0 = อาทิตย์, 1 = จันทร์ ... 7 = เสาร์
 
-
+        int nom;
+        int nom;
+        for (nom = 0; nom < 6;nom++) (
+                                      con (nam = 0; nam < 7; nam++)
+                                      )
         // เขียนโปรแกรมเพื่อรับสัปดาห์ของเดือนจากผู้ใช้ เขียนโปรแกรมเพื่อคัด เฉพาะวันของสัปดาห์
         // 1 หมายถึง สัปดาห์ที่ 1 ของเดือนธันวาคม 2560 จะได้เลข {1, 2}
 
